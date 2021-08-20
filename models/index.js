0e865a1c6d46314f3b4e327514d381d282cf8b64
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-20 14:37:51 -0500

    fixing delete to cascade

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const Comment = require('./Comment');
 
 //create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
 });
 
 Post.belongsTo(User, {
@@ -12,19 +12,21 @@ Post.belongsTo(User, {
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: "cascade"
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+    foreignKey: 'post_id',
+    onDelete: "cascade"
 });
 
 module.exports = { User, Post, Comment };
---
+++ state-city.js
@@ -0,0 +1,11 @@
+// // Latest version - v3.0.0
+// let Country = require('country-state-city').Country;
+// let State = require('country-state-city').State;
+// let City = require('country-state-city').City;
+let csc = require('country-state-city').default;
+
+// console.log(Country.getAllCountries())
+// console.log(State.getAllStates())
+
+
+console.log(csc.getCountryByCode(AS))
